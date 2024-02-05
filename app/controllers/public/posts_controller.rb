b4164ed616ace97aaf148e751dd995c3db329f48
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-02-05 11:33:27 +0000

    Add 新規投稿、投稿詳細

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,3 +1,44 @@
 class Public::PostsController < ApplicationController
     before_action :authenticate_user!
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        if @post.save
+            redirect_to post_path(@post)
+        else
+            flash[:notice] = "画像を選択してください"
+            render :new
+        end
+    end
+    
+    def index
+        @post = Post.all
+    end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    def draft
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:posted_title, :post_content, :image)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    has_one_attached :image
+    validate :image_attached
+    
+    def get_image(width, height)
+        image.variant(resize_to_limit: [width, height]).processed
+    end
+    
+    private
+    
+    def image_attached
+        unless image.attached?
+            errors.add(:image, "画像を選んでください")
+        end
+    end
 end
