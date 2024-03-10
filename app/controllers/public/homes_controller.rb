57611c9f9a517e1f786c7638320572d8489ae945
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-03-10 08:58:35 +0000

    Add フォント Fix top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -5,6 +5,9 @@ class Public::HomesController < ApplicationController
         from = Time.current.at_beginning_of_day
         @post_today = Post.includes(:laughed_buttons).max_by { |post| post.laughed_buttons.where(created_at: from...to).count }
         @posts = Post.published.order(created_at: :desc).limit(6)
+        
+        # @post_todayがnilの場合
+        @post_today_text = @post_today.nil?? "今日はお休みだよ！" :nil
     end
     
     def about
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,6 @@
 class Follower < ApplicationRecord
     belongs_to :user
+    #belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+    belongs_to :target_user, class_name: 'User', foreign_key: 'follow_id' 
     
 end
