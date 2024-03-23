9e51212733ffc5cdf53fc7cb6623af02dc5d3b00
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-03-23 09:46:15 +0000

    コメント

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
     has_many :notifications, dependent: :destroy
-    belongs_to :user
     belongs_to :post
+    belongs_to :user
     
     validates :comment_content, presence: true
 end
