597958ff52c19c1807e7bddd394adf1113588a15
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-05-28 11:48:35 +0000

    commit確認のためのcommit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-    
-    
     before_action :authenticate_user!, except: [:top, :about], if: :check_admin
-    
     
     def check_admin
         if admin_signed_in?
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     has_one_attached :image
-    has_many :comments, dependent: :destroy
     has_many :laughed_buttons, dependent: :destroy
+    has_many :comments, dependent: :destroy
     has_many :post_tags, dependent: :destroy
     has_many :notifications, dependent: :destroy
     has_many :tags, through: :post_tags
